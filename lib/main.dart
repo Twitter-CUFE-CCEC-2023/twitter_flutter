babd0ee2c99cf9072e20106f765757cd2bfde777
khaled128 <63272346+khaled128@users.noreply.github.com>
2022-03-20 20:23:21 +0200

    createAcccount screen1 - not finished yet
    fix asset problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_flutter/screens/CreateAccount1.dart';
 import 'package:twitter_flutter/screens/firstscreen.dart';
 import 'package:twitter_flutter/screens/secondscreen.dart';
 
@@ -8,12 +9,14 @@ void main() {
       title: 'Named Routes Demo',
       // Start the app with the "/" named route. In this case, the app starts
       // on the FirstScreen widget.
-      initialRoute: FirstScreen.route,
+      initialRoute: CreateAccount1.route,
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
         FirstScreen.route: (context) => const FirstScreen(),
         // When navigating to the "/second" route, build the SecondScreen widget.
         SecondScreen.route: (context) => const SecondScreen(),
+
+        CreateAccount1.route: ((context) => const CreateAccount1()),
       },
     ),
   );
