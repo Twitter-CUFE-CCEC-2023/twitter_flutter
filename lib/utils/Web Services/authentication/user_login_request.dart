09143e938babe5ba59631d1c870bf8ecd0cb329f
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-04-18 04:15:18 +0200

    edit the login function in login_request
    to throw an exception with an appropriate error message based on the server response code

--- lib/utils/Web Services/authentication/user_login_request.dart
+++ lib/utils/Web Services/authentication/user_login_request.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:twitter_flutter/utils/Web Services/constants.dart';
 
@@ -9,7 +10,7 @@ class UserLoginRequest {
 
   UserLoginRequest(this._email_or_username, this._password);
 
-  Future<String> Login({username,password}) async {
+  Future<String> Login({username, password}) async {
     var headers = {'Content-Type': 'application/json'};
 
     //TODO:Correct post request body to be used upon deployment
@@ -20,18 +21,25 @@ class UserLoginRequest {
     });*/
 
     //TODO:Josn-Server-auth request body format to be deleted upon deployment
-    var body = jsonEncode(<String,String>{
+    var body = jsonEncode(<String, String>{
       "email": username ?? _email_or_username,
       "password": password ?? _password
     });
 
     http.Response res = await http.post(Uri.parse("$ENDPOINT/auth/login"),
         body: body, headers: headers);
-    if (res.statusCode == 200) {
+
+    int statusCode = res.statusCode;
+    if (statusCode == 200) {
       return res.body;
+    } else if (statusCode == 400) {
+      throw Exception("Client Error, Can not process your request");
+    } else if (statusCode == 401) {
+      throw Exception("Invalid Login Credentials");
+    } else if (statusCode == 500) {
+      throw Exception("Server Error");
     } else {
-      String? errorMessage = "Login Error: " + res.reasonPhrase !+ "-StatusCode:" +res.statusCode.toString() ;
-      throw Exception(errorMessage);
+      throw Exception("Undefined Error");
     }
   }
 }
