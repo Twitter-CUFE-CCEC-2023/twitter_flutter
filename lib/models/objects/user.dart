91e93f8a1155eaf9f7bd66762a697a0c98379f80
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-04-24 14:30:02 +0200

    delete UserModel Extra params

--- lib/models/objects/user.dart
+++ lib/models/objects/user.dart
@@ -1,32 +1,11 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 
 class UserModel extends Equatable{
+  late String _id;
   late String name;
   late String username;
   late String email;
-  late DateTime birth_date;
-  late String gender;
-  late String location;
-  late String bio;
-  late List<UserModel> followers;
-  late List<UserModel> followings;
-  late String website;
-  late int verificationCode ;
-  late DateTime verificationCodeExpiration;
-  late int resetPasswordCode;
-  late DateTime resetPasswordCodeExpiration;
-  late bool isVerified;
-  late String profile_picture;
-  late String cover_picture;
-  late bool isBanned;
-  late String monthDayBirthAccessId;
-  late String yearBirthAccessId;
-  late int _id;
-  late DateTime createdAt;
-  late DateTime updatedAt;
 
-  //__v,tokens[]
 
   UserModel.fromJson(Map<String,dynamic> json)
   {
@@ -36,6 +15,5 @@ class UserModel extends Equatable{
   }
 
   @override
-  // TODO: implement props
   List<Object?> get props => [name,username,email,];
 }
