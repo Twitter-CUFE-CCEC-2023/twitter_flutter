02c14709859f15c1ceaa6b42393fad2831be00b4
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-05-09 15:02:11 +0200

    fix tab bar not being pinned in profile page and fetch tweets for the first tab

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:twitter_flutter/blocs/userManagement/user_management_states.dart';
 import 'package:twitter_flutter/models/objects/user.dart';
-import 'package:twitter_flutter/screens/profile/edit_profile.dart';
 import 'package:twitter_flutter/screens/profile/pre_edit_profile.dart';
 import 'package:twitter_flutter/screens/profile/profile_page_tabs/likes.dart';
 import 'package:twitter_flutter/screens/profile/profile_page_tabs/media.dart';
@@ -56,97 +57,74 @@ class _UserProfileState extends State<UserProfile> {
     var height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverPersistentHeader(
-              delegate: MySliverAppBar(
-                  coverImageURL: userData.cover_image_url,
-                  profileImageURL: userData.profile_image_url,
-                  name: userData.name,
-                  tweetCount: userData.tweets_count,
-                  expandedHeight: orientation == Orientation.portrait
-                      ? 0.35 * width
-                      : 0.625 * height),
-              pinned: true,
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                padding: const EdgeInsets.only(top: 3),
-                color: Colors.white,
-                child: SizedBox(
-                    child: Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      OutlinedButton(
-                        child: const Text("Edit Profile"),
-                        style: outlinedButtonsStyle,
-                        onPressed: () =>
-                            Navigator.pushNamed(context, PreEditProfile.route),
-                      ),
-                    ],
-                  ),
-                )),
+        body: DefaultTabController(
+          length: 4,
+          child: CustomScrollView(
+            slivers: [
+              SliverPersistentHeader(
+                delegate: MySliverAppBar(
+                    coverImageURL: userData.cover_image_url,
+                    profileImageURL: userData.profile_image_url,
+                    name: userData.name,
+                    tweetCount: userData.tweets_count,
+                    expandedHeight: orientation == Orientation.portrait
+                        ? 0.35 * width
+                        : 0.625 * height),
+                pinned: true,
               ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                color: Colors.white,
-                child: SizedBox(
-                    child: _buildUserData(
-                  name: userData.name,
-                  username: userData.username,
-                  birthday: userData.birth_date.day,
-                  birthmonth: DateFormat('MMMM')
-                      .format(DateTime(0, userData.birth_date.month)),
-                  birthyear: userData.birth_date.year,
-                  followers: userData.followers_count,
-                  following: userData.following_count,
-                  joinMonth: DateFormat('MMMM')
-                      .format(DateTime(0, userData.created_at.month)),
-                  joinYear: userData.created_at.year,
-                )),
+              SliverToBoxAdapter(
+                child: Container(
+                  padding: const EdgeInsets.only(top: 3),
+                  color: Colors.white,
+                  child: SizedBox(
+                      child: Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        OutlinedButton(
+                          child: const Text("Edit Profile"),
+                          style: outlinedButtonsStyle,
+                          onPressed: () =>
+                              Navigator.pushNamed(context, PreEditProfile.route),
+                        ),
+                      ],
+                    ),
+                  )),
+                ),
               ),
-            ),
-            SliverFillRemaining(
-              hasScrollBody: true,
-              child: DefaultTabController(
-                length: 4,
+              SliverToBoxAdapter(
+                child: Container(
+                  color: Colors.white,
+                  child: SizedBox(
+                      child: _buildUserData(
+                    name: userData.name,
+                    username: userData.username,
+                    birthday: userData.birth_date.day,
+                    birthmonth: DateFormat('MMMM')
+                        .format(DateTime(0, userData.birth_date.month)),
+                    birthyear: userData.birth_date.year,
+                    followers: userData.followers_count,
+                    following: userData.following_count,
+                    joinMonth: DateFormat('MMMM')
+                        .format(DateTime(0, userData.created_at.month)),
+                    joinYear: userData.created_at.year,
+                  )),
+                ),
+              ),
+
+              showSliverAppBar(),
+
+              SliverFillRemaining(
+                hasScrollBody: true,
                 child: Scaffold(
-                  appBar: TabBar(
-                    isScrollable: true,
-                    labelPadding: EdgeInsets.symmetric(horizontal: 20),
-                    tabs: [
-                      Tab(
-                          child: Text(
-                        "Tweets",
-                        style: TextStyle(color: Colors.black),
-                      )),
-                      Tab(
-                          child: Text(
-                        "Tweets & replies",
-                        style: TextStyle(color: Colors.black),
-                      )),
-                      Tab(
-                          child: Text(
-                        "Media",
-                        style: TextStyle(color: Colors.black),
-                      )),
-                      Tab(
-                          child: Text(
-                        "Likes",
-                        style: TextStyle(color: Colors.black),
-                      )),
-                    ],
-                  ),
                   body: TabBarView(
                     children: [Tweets(), TweetsAndReplies(), Media(), Likes()],
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
@@ -350,12 +328,13 @@ class MySliverAppBar extends SliverPersistentHeaderDelegate {
     );
   }
 
+
   @override
   double get maxExtent => expandedHeight;
   @override
   double get minExtent => kToolbarHeight;
   @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => false;
 
   double profileImageSize(BuildContext context, double opacityFactor,
       double shrinkOffset, double maxSize,
@@ -500,3 +479,43 @@ class MySliverAppBar extends SliverPersistentHeaderDelegate {
     );
   }
 }
+
+//test
+SliverAppBar showSliverAppBar() {
+  return SliverAppBar(
+    leading: Icon(null),
+    leadingWidth: 0,
+    backgroundColor: Colors.white,
+    floating: false,
+    pinned: true,
+    snap: false,
+    toolbarHeight: 50,
+    title: TabBar(
+      isScrollable: true,
+      labelPadding: EdgeInsets.symmetric(horizontal: 20),
+      tabs: [
+        Tab(
+            child: Text(
+              "Tweets",
+              style: TextStyle(color: Colors.black),
+            )),
+        Tab(
+            child: Text(
+              "Tweets & replies",
+              style: TextStyle(color: Colors.black ),
+            )),
+        Tab(
+            child: Text(
+              "Media",
+              style: TextStyle(color: Colors.black),
+            )),
+        Tab(
+            child: Text(
+              "Likes",
+              style: TextStyle(color: Colors.black),
+            )),
+      ],
+    ),
+  );
+}
+
--- lib/screens/profile/profile_page_tabs/tweets.dart
+++ lib/screens/profile/profile_page_tabs/tweets.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:twitter_flutter/blocs/tweetsStates/tweets_managment_bloc.dart';
+import 'package:twitter_flutter/blocs/tweetsStates/tweets_managment_states.dart';
+import 'package:twitter_flutter/blocs/tweetsStates/tweets_management_events.dart';
+import 'package:twitter_flutter/blocs/userManagement/user_management_bloc.dart';
+import '../../../models/objects/tweet.dart';
 import '../tweets_widget.dart';
 
 class Tweets extends StatefulWidget {
@@ -11,56 +17,47 @@ class Tweets extends StatefulWidget {
 class _TweetsState extends State<Tweets> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    return ListView(
-      children: [
-        tweet(
-          userProfilePicture:
-              "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2017/03/28/Local-Politics/Images/Supreme_Court_Gorsuch_Moments_22084-70c71-0668.jpg?t=20170517",
-          user_Name: "Johnny",
-          screenHeight: screenHeight,
-          screenWidth: screenWidth,
-          imageCount: 0,
-          CommentCount: 2,
-          retweetCount: 4,
-          likeCount: 7,
-          tweet_Text: "Hello guys, How are you?",
-        ),
-        tweet(
-            userProfilePicture:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQmyyuPaZzRHAIpnCtIWLhyIoghmcPu3dZxQ&usqp=CAU",
-            user_Name: "Activation",
-            screenHeight: screenHeight,
-            screenWidth: screenWidth,
-            imageCount: 4,
-            CommentCount: 20,
-            retweetCount: 40,
-            likeCount: 200,
-            tweet_Text:
-                "Check out the newest mapes in the game (new enemies added, new characters, and new guns)",
-            imageOne:
-                "https://cdn.vox-cdn.com/thumbor/v4CFyRhEvWB9Ct_YeP8tEH0i2xo=/0x0:1920x1080/1200x800/filters:focal(381x260:687x566)/cdn.vox-cdn.com/uploads/chorus_image/image/68764501/FirebaseZ2.0.jpg",
-            imageTwo:
-                "https://imageio.forbes.com/specials-images/imageserve/60e7537510a61c82e917781b/BOCW-Zombies-Story-So-Far-TOUT/960x0.jpg?fit=bounds&format=jpg&width=960",
-            imageThree:
-                "https://gamingintel.com/wp-content/uploads/2020/11/Black-Ops-Cold-War-New-Map-Vietnam-Zombies.jpg",
-            imageFour:
-                "https://charlieintel.com/wp-content/uploads/2021/06/mauer-der-toten-1.jpg"),
-        tweet(
-          userProfilePicture:
-              "https://pbs.twimg.com/media/E9gpNWsXEAYir33.jpg",
-          user_Name: "Maged Alosali",
-          screenHeight: screenHeight,
-          screenWidth: screenWidth,
-          tweet_Text: "Check the new gif",
-          imageCount: 1,
-          CommentCount: 2,
-          retweetCount: 3,
-          likeCount: 4,
-          imageOne: "https://c.tenor.com/EWiHVwPUEOoAAAAC/coxa-among-us.gif",
-        ),
-      ],
-    );
+    var userbloc = context.read<UserManagementBloc>();
+    var tweetbloc = context.read<TweetsManagementBloc>();
+    tweetbloc.add(UserProfileTweetsTabOpen(username: userbloc.userdata.username,access_token: userbloc.access_token));
+    return BlocConsumer<TweetsManagementBloc, TweetsManagementStates>(
+        builder: (context, state) {
+      if (state is TweetsLoadingState) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if (state is SuccessLoadingUserProfileTweetsTab) {
+        var user = userbloc.userdata;
+        double ScreenWidth = MediaQuery.of(context).size.width;
+        double ScreenHeight = MediaQuery.of(context).size.height;
+        List<Widget> tweetsList = [];
+        for (TweetModel currentTweet
+            in tweetbloc.LoggedUserTweetsWithoutReplies) {
+          tweetsList.add(tweet(
+            screenWidth: ScreenWidth,
+            screenHeight: ScreenHeight,
+            CommentCount: currentTweet.replies_count,
+            imageCount: currentTweet.media.length,
+            likeCount: currentTweet.likes_count,
+            retweetCount: currentTweet.retweets_count,
+            user_Name: user.username,
+            userProfilePicture: user.profile_image_url,
+            tweet_Text: currentTweet.content,
+          ));
+        }
+        return ListView(
+          children: tweetsList,
+        );
+      } else
+        return Container();
+    }, listener: (context, state) {
+      if (state is FailureLoadingUserProfileTweetsTab) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(state.errorMessage),
+          ),
+        );
+      }
+    });
   }
 }
