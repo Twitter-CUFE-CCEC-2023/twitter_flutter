a80ad5186afd1874864a0cf77b45ae77e2937f9e
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-05-09 14:59:56 +0200

    add menu to the edit button when the image is opened

--- lib/screens/utility_screens/opened_image.dart
+++ lib/screens/utility_screens/opened_image.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:focused_menu/modals.dart';
 import 'package:twitter_flutter/widgets/authentication/constants.dart';
+import 'package:focused_menu/focused_menu.dart';
 
 class OpenedImage extends StatelessWidget {
   static const String route = "/OpenedImage";
@@ -32,21 +34,41 @@ class OpenedImage extends StatelessWidget {
           ],
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(child: Image.network(imageURL)),
-            Center(
-              child: OutlinedButton(
-                onPressed: () {},
-                style: outlinedButtonsStyle.copyWith(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                  elevation: MaterialStateProperty.all<double>(5),
+            Padding(
+              padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.12),
+              child: FocusedMenuHolder(
+                blurSize: 0,
+                animateMenuItems: false,
+                //bottomOffsetHeight: MediaQuery.of(context).size.height/2,
+                menuOffset: MediaQuery.of(context).size.height/2.5,
+                menuWidth: MediaQuery.of(context).size.width*0.8,
+                duration: Duration.zero,
+                openWithTap: true,
+                child:  Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(right:MediaQuery.of(context).size.width*0.12),
+                    child: OutlinedButton(
+                      style: outlinedButtonsStyle.copyWith(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                        elevation: MaterialStateProperty.all<double>(5),
+                      ),
+                      onPressed: null,
+                      child:Text("Edit"),
+                    ),
+                  ),
                 ),
-                child: Text("Edit"),
+                menuItems: [
+                  FocusedMenuItem(title: Text("Take photo"), onPressed: (){}),
+                  FocusedMenuItem(title: Text("Choose Existing Photo"), onPressed: (){}),
+                ],
+                onPressed: (){},
               ),
-            )
+            ),
           ],
         ),
       ),
